7348a5e3572497f76290b1a03e09058d37f28794
patrick_welter <pa.welter@web.de>
2020-12-24 19:12:45 +0100

    Added

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BoxGeometry, Mesh, MeshBasicMaterial, PerspectiveCamera, Scene, WebGLRenderer } from 'three';
 
 @Component({
   selector: 'app-game',
@@ -7,7 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameComponent implements OnInit {
 
-  constructor() { }
+  private scene = new Scene()
+  private camera = new PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 )
+  private renderer = new WebGLRenderer()
+
+  private geometry = new BoxGeometry
+  private material = new MeshBasicMaterial({color: 0x00ff00})
+  private cube = new Mesh(this.geometry, this.material)
+
+
+  constructor() {
+    this.renderer.setSize(window.innerWidth, window.innerHeight)
+    document.body.appendChild(this.renderer.domElement)
+
+    this.scene.add(this.cube)
+
+    this.camera.position.z = 5
+
+    this.animate()
+   }
+
+  private animate = () => {
+    requestAnimationFrame(this.animate)
+
+    this.cube.rotation.x += 0.01
+    this.cube.rotation.y += 0.01
+
+    this.renderer.render(this.scene, this.camera)
+  }
 
   ngOnInit(): void {
   }
